10df06fc5cecf1c929fa08a2ec2d32bd471e7d07
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2024-10-24 17:09:10 +0300

    initial commit - Chapter 7: Relations Between Models

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+  'name': 'Real Estate',
+  'license': 'LGPL-3',
+  'application': True,
+  'data': [
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml',
+    'views/estate_property_type_views.xml',
+    'views/estate_menus.xml'
+  ]
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,45 @@
+from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Properties"
+
+    name = fields.Char(required=True, string="Title")
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        copy=False,
+        default=fields.Date.today() + relativedelta(months=3),
+        string="Available From",
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        default="new",
+    )
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
