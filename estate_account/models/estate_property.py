354e06ef3fc27fd30c80bf081504a49f688ede7f
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-01-12 09:14:07 +0300

    Server framework 101 - Chapter 13: Interact With Other Modules

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import models, Command
+
+
+class EstateProperty(models.Model):
+    _inherit = ["estate.property"]
+
+    def action_sell_property(self):
+        self.env["account.move"].sudo().with_context(
+            default_move_type="out_invoice"
+        ).create(
+            {
+                "partner_id": self.env.user.id,
+                "line_ids": [
+                    Command.create(
+                        {
+                            "name": "Commission",
+                            "quantity": 1,
+                            "price_unit": self.selling_price * 0.06,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            "name": "Administrative Fees",
+                            "quantity": 1,
+                            "price_unit": 100,
+                        }
+                    ),
+                ],
+            }
+        )
+
+        return super().action_sell_property()
